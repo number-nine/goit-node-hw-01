50530b57e2b9be59aa0a807426f78c51c1492f23
number-nine <sergii.pokhylko@gmail.com>
2023-07-06 15:41:54 +0300

    mvp

--- contacts.js
+++ contacts.js
@@ -1,12 +1,8 @@
 const fs = require("fs/promises");
 const path = require("path");
-const {nanoid} = require('nanoid');
+const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "db\\contacts.json");
-
-console.log(contactsPath);
-
-
 
 module.exports = {
   listContacts,
@@ -16,38 +12,35 @@ module.exports = {
 };
 
 async function listContacts() {
-    const response = await fs.readFile(contactsPath, 'utf-8');
-    const contacts = JSON.parse(response);
-    return contacts;
-    
+  const response = await fs.readFile(contactsPath, "utf-8");
+  const contacts = JSON.parse(response);
+  return contacts;
 }
 
 async function getContactById(contactId) {
-    const contacts = await listContacts();
-    const contact = contacts.find(contact => contact.id === contactId);
-    return contact || null;
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+  return contact || null;
 }
 
 async function removeContact(contactId) {
-    const contacts = await listContacts(); 
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) return null;
+  const removedContact = contacts.splice(index, 1)[0];
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 1));
+  return removedContact;
 }
 
 async function addContact(name, email, phone) {
-    const contacts = await listContacts();
-    const newContact = {
-        id: nanoid(),
-        name,
-        email,
-        phone,
-    }
-    contacts.push(newContact);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 1));
-    return contacts;
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 1));
+  return newContact;
 }
-
-
-addContact("new name", "test@test", "67367622203").then((res) => {
-  console.log("-----------------");
-//   console.log(nanoid());
-//   console.log(res);
-});
--- index.js
+++ index.js
@@ -1,4 +1,39 @@
-const contacts = require('./contacts');
+const { Command } = require("commander");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
 
-console.log('ok');
- 
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      return console.table(await listContacts());
+    case "get":
+      return console.log(await getContactById(id));
+
+    case "add":
+      return console.log(await addContact(name, email, phone));
+
+    case "remove":
+      return console.log(await removeContact(id));
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
